7d89bad7628b046fd41962471f209886d45e8b83
javier0403 <vancedjavier0403@gmail.com>
2024-07-23 04:01:22 -0400

    se edita la edad nominal

--- bdapp/views.py
+++ bdapp/views.py
@@ -1,12 +1,11 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from .models import TransformedData
-from .serializers import TransformedDataSerializer
 import os
 import mysql.connector
 from django.conf import settings
 import os
 from django.http import JsonResponse
+from datetime import date
 
 @api_view(['POST'])
 def transform_data(request):
@@ -29,22 +28,33 @@ def transform_data(request):
         if conexion.is_connected():
             cursor = conexion.cursor()
 
-            # Obtener la consulta SQL desde la variable de entorno
-            insert_query = os.getenv('INSERT_QUERY')
+            # Obtener todos los datos de la tabla raw_data
+            cursor.execute("SELECT id, nombre, apellido, edad FROM raw_data")
+            raw_data = cursor.fetchall()
 
-            # Ejecutar la consulta
-            cursor.execute(insert_query)
+            # Transformar los datos
+            for data in raw_data:
+                id, nombre, apellido, fecha_nacimiento = data
+                nombre_completo = f"{nombre} {apellido}"
+                edad_nominal = (date.today() - fecha_nacimiento).days // 365
+
+                # Insertar los datos transformados en la tabla transformed_data
+                insert_query = """
+                INSERT INTO transformed_data (nombre_completo, edad_nominal)
+                VALUES (%s, %s)
+                """
+                cursor.execute(insert_query, (nombre_completo, edad_nominal))
+
+            # Confirmar los cambios
             conexion.commit()
-
             cursor.close()
             conexion.close()
 
-            return JsonResponse({'status': 'success', 'message': 'Datos insertados correctamente'})
+            return JsonResponse({'status': 'success', 'message': 'Datos transformados e insertados correctamente'})
         else:
             return JsonResponse({'status': 'error', 'message': 'No se pudo conectar a la base de datos'})
     except mysql.connector.Error as error:
         return JsonResponse({'status': 'error', 'message': f'Error: {error}'})
-
 @api_view(['GET'])
 def get_transformed_data(request):
     try:
